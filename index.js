f8d8d4bd89e10ca69a1f598cab37170833548488
Amir Sadiq Ali <33599273+ammy-bajwa@users.noreply.github.com>
2020-03-06 18:04:14 +0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const electron = require("electron");
+
+const { app, BrowserWindow, Menu } = electron;
+
+let mainBrowserWindow, addWindow;
+
+app.on("ready", () => {
+  mainBrowserWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  mainBrowserWindow.loadURL(`file://${__dirname}/index.html`);
+
+  const mainMenu = Menu.buildFromTemplate(menuTemplate);
+  Menu.setApplicationMenu(mainMenu);
+});
+
+const addTodoWindow = () => {
+  addWindow = new BrowserWindow({
+    width: 400,
+    height: 300,
+    title: "Add New Todo"
+  });
+};
+
+const menuTemplate = [
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "Add Todo",
+        click() {
+          addTodoWindow();
+        }
+      },
+      {
+        label: "Exit",
+        accelerator: (() => {
+          if (process.platform === "darwin") {
+            return "Command+Q";
+          } else {
+            return "Ctrl+Q";
+          }
+        })(),
+        click() {
+          app.quit();
+        }
+      }
+    ]
+  }
+];
